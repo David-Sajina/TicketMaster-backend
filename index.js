d451a833d0d6e37475e35e20fb20f37ee7c732cb
David-Sajina <57251489+David-Sajina@users.noreply.github.com>
2022-05-09 14:04:04 +0200

    Backend update

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 import express from "express";
+import cors from "cors";
 const port = 5000;
 console.log(port);
 const app = express();
-let test = { test: "test" };
+app.use(cors());
+let user = [
+	{ username: "Andrej", password: "Korica" },
+	{ username: "David", password: "Šajina" },
+	{ username: "Alen", password: "Valek" },
+	{ username: "Maja", password: "Vrh" },
+	{ username: "Dominik", password: "Ružić" },
+	{ username: "Deni", password: "Vidan" },
+];
 
+app.get(`/:name`, (req, res) => {
+	const { name } = req.params;
+	user.forEach((el) => {
+		if (el.username == name) {
+			res.send(el);
+		}
+	});
+});
 app.get("/", (req, res) => {
-	res.send(test);
+	res.send(user);
 });
 
 app.listen(port, () => console.log(`Slušam zahtjeve http://localhost:${port}`));
