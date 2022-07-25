63930dde62802271ada9c55b7f095517e0f3fec8
David-Sajina <57251489+David-Sajina@users.noreply.github.com>
2022-07-25 22:01:31 +0200

    add user to schema, +get ticket

--- index.js
+++ index.js
@@ -18,133 +18,140 @@ app.use(express.json());
 app.use(cors());
 
 const connectToDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to mongo");
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		await mongoose.connect(process.env.MONGO_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+		console.log("Connected to mongo");
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 await connectToDB();
 
 app.get("/auth", auth, async (req, res) => {
-  try {
-    console.log(req.user, "aaaaa");
-    res.json(req.user);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Server error" });
-  }
+	try {
+		/* console.log(req.user, "aaaaa"); */
+		res.json(req.user);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ error: "Server error" });
+	}
 });
 
 app.post(`/ticket`, async (req, res) => {
-  const { name, location, start, question, answer } = req.body;
-  try {
-    let newTicket = new TicketHeader({
-      name,
-      location,
-      start,
-      question,
-      answer,
-    });
-    await newTicket.save();
-    console.log(newTicket);
-  } catch (error) {
-    console.log(error);
-  }
-  res.send("OK");
+	const { user, name, location, start, question, answer } = req.body;
+	try {
+		let newTicket = new TicketHeader({
+			user,
+			name,
+			location,
+			start,
+			question,
+			answer,
+		});
+		await newTicket.save();
+		console.log(newTicket);
+	} catch (error) {
+		console.log(error);
+	}
+	res.send("OK");
+});
+app.get(`/ticket`, auth, async (req, res) => {
+	try {
+		let tickets = await TicketHeader.find({ user: req.user.email });
+		res.send(tickets);
+	} catch (error) {}
 });
 
 app.post(`/`, (req, res) => {
-  const { username, password } = req.body;
-  user.push({ username, password });
-  res.send("OK");
+	const { username, password } = req.body;
+	user.push({ username, password });
+	res.send("OK");
 });
 
 app.post(`/register`, async (req, res) => {
-  const { email, username, password } = req.body;
+	const { email, username, password } = req.body;
 
-  // provjere lozinke / username / mail
+	// provjere lozinke / username / mail
 
-  try {
-    let user = await User.findOne({ email });
-    if (user) return res.status(400).json({ error: "email already in use." });
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+	try {
+		let user = await User.findOne({ email });
+		if (user) return res.status(400).json({ error: "email already in use." });
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(password, salt);
 
-    user = new User({
-      email,
-      username,
-      password: hashedPassword,
-    });
+		user = new User({
+			email,
+			username,
+			password: hashedPassword,
+		});
 
-    await user.save();
+		await user.save();
 
-    const payload = {
-      user: {
-        id: user._id,
-      },
-    };
-    console.log(user);
-    let token = jwt.sign(
-      payload,
-      process.env.SECRET_KEY,
-      {
-        expiresIn: 3600,
-      },
-      (error, token) => {
-        if (error) throw error;
-        res.json({ token });
-      }
-    );
-    console.log(token, "token");
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Server error" });
-  }
+		const payload = {
+			user: {
+				id: user._id,
+			},
+		};
+		/* console.log(user); */
+		let token = jwt.sign(
+			payload,
+			process.env.SECRET_KEY,
+			{
+				expiresIn: 3600,
+			},
+			(error, token) => {
+				if (error) throw error;
+				res.json({ token });
+			}
+		);
+		console.log(token, "token");
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ error: "Server error" });
+	}
 });
 
 app.post("/login", async (req, res) => {
-  const { username, password } = req.body;
+	const { username, password } = req.body;
 
-  try {
-    let user = await User.findOne({ username });
-    if (!user) {
-      return res.status(400).json({ error: "Invalid username" });
-    }
-    const userPass = await bcrypt.compare(password, user.password);
-    if (!userPass) {
-      return res.status(400).json({ error: "Invalid credentials" });
-    }
-    let userObj = await User.findOne({ username }).select("-password");
+	try {
+		let user = await User.findOne({ username });
+		if (!user) {
+			return res.status(400).json({ error: "Invalid username" });
+		}
+		const userPass = await bcrypt.compare(password, user.password);
+		if (!userPass) {
+			return res.status(400).json({ error: "Invalid credentials" });
+		}
+		let userObj = await User.findOne({ username }).select("-password");
 
-    const payload = {
-      user: {
-        id: user.id,
-        username: user.username,
-        email: user.email,
-      },
-    };
+		const payload = {
+			user: {
+				id: user.id,
+				username: user.username,
+				email: user.email,
+			},
+		};
 
-    jwt.sign(
-      payload,
-      process.env.SECRET_KEY,
-      {
-        expiresIn: 3600,
-      },
-      (err, token) => {
-        if (err) throw err;
-        res.json({ token });
-      }
-    );
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Server error" });
-  }
+		jwt.sign(
+			payload,
+			process.env.SECRET_KEY,
+			{
+				expiresIn: 3600,
+			},
+			(err, token) => {
+				if (err) throw err;
+				res.json({ token });
+			}
+		);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ error: "Server error" });
+	}
 });
 
 app.listen(port, () => console.log(`Slušam zahtjeve http://localhost:${port}`));
--- models/TicketHeader.js
+++ models/TicketHeader.js
@@ -1,31 +1,35 @@
 import mongoose from "mongoose";
 
 const TicketHeaderSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    location: {
-      type: String,
-      required: true,
-    },
-    start: {
-      type: Date,
-      required: true,
-    },
-    question: {
-      type: String,
-      required: true,
-    },
-    answer: {
-      type: String,
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-  }
+	{
+		user: {
+			type: String,
+			required: true,
+		},
+		name: {
+			type: String,
+			required: true,
+		},
+		location: {
+			type: String,
+			required: true,
+		},
+		start: {
+			type: Date,
+			required: true,
+		},
+		question: {
+			type: String,
+			required: true,
+		},
+		answer: {
+			type: String,
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
 );
 
 const TicketHeader = mongoose.model("ticketheader", TicketHeaderSchema);
